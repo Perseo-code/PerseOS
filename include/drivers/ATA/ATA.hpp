56de42207016714e6889f1f9c60ba942d99e3e48
Perseo-code <ayuskyap@gmail.com>
2026-07-31 11:52:54 +0200

    Added ATA drivers (not implemented in the shell yet)

---
+++ include/drivers/ATA/ATA.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <io/io.hpp>
+#include <error/error.hpp>
+#include <stdint.hpp>
+class ATA {
+private:
+    void cacheFlush() {
+        outb(0x1F7, 0xE7);
+    }
+
+    void setLBAbits(uint32_t lba) {
+        outb(0x1F6, 0xE0 | ((lba >> 24) & 0x0F));
+        outb(0x1F2, 1);
+        outb(0x1F3, lba & 0xFF);
+        outb(0x1F4, (lba >> 8) & 0xFF);
+        outb(0x1F5, (lba >> 16) & 0xFF);
+    }
+public:
+    bool identify() {
+        outb(0x1F6, 0xA0);
+        outb(0x1F7, 0xEC);
+
+        uint8_t status = inb(0x1F7);
+
+        if (status == 0) {
+            RAISE(DeviceErr, ERR_DRIVE_NOT_FOUND, false, "");
+            return false;
+        }
+        return true;
+    }
+
+    void read28(uint32_t lba, uint8_t* buffer) {
+        setLBAbits(lba);
+        outb(0x1F7, 0x20); // READ SECTORS
+        
+        for (int i = 0; i < 256; i++) {
+            ((uint16_t*)buffer)[i] = inw(0x1F0); // Data port
+        }
+    }
+
+    void write28(uint32_t lba, const uint8_t* buffer) {
+        setLBAbits(lba);
+        outb(0x1F7, 0x30);
+        for (int i = 0; i < 256; i++) {
+            outw(0x1F0, ((uint16_t*)buffer)[i]);
+        }
+        cacheFlush(); // CACHE FLUSH
+    }
+};
--- include/io/io.hpp
+++ include/io/io.hpp
@@ -32,10 +32,24 @@ inline void outb(uint16_t port, uint8_t value)
         : "a"(value), "Nd"(port));
 }
 
+inline void outw(uint16_t port, uint16_t value) {
+    asm volatile ("outb %0, %1"
+        :
+        : "a"(value), "Nd"(port));
+}
+
 inline uint8_t inb(uint16_t port)
 {
     uint8_t value;
     asm volatile ("inb %1, %0"
+        : "=a"(value)
+        : "Nd"(port));
+    return value;
+}
+
+inline uint16_t inw(uint16_t port) {
+    uint16_t value;
+    asm volatile ("inw %1, %0"
         : "=a"(value)
         : "Nd"(port));
     return value;
